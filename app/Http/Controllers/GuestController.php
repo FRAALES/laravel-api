2546383cef9dcb1c944932829218c326d64d77b3
FRAALES <alessandrofranco1992@gmail.com>
2023-08-21 19:31:43 +0200

    nomi projects stampati

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+
 class GuestController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $projects = Project::all();
+        return view('home', compact('projects'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Hello World!</h1>
+<div class="container pt-5 text-center">
+    <h1>Hello World!</h1>
+    <ul class="list-unstyled">
+        @foreach ($projects as $project)
+        <li>{{$project -> name}}</li>
+        @endforeach
+    </ul>
+</div>
+
 @endsection
