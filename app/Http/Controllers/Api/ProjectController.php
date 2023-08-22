ef3535918a67190a13342e1a48bf156b94ac44e3
FRAALES <alessandrofranco1992@gmail.com>
2023-08-22 17:27:20 +0200

    chiamata postman non funziona

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function testApi()
     {
         $projects = Project::all();
         return response()->json([
